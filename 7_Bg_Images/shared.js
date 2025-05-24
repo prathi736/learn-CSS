6f59f83ae225d78a303b04a47f4997df58c0d45c
Pulkit Rathi <pulkitr2002@gmail.com>
2025-05-24 21:28:13 +0530

    modal js feature

--- 7_Bg_Images/shared.js
+++ 7_Bg_Images/shared.js
@@ -1,5 +1,6 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoBtn = document.querySelector('.modal_action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 
 // console.dir(backdrop);
@@ -12,3 +13,24 @@ for (var i=0; i<selectPlanButtons.length; i++) {
         backdrop.style.display = 'block';
     });
 }
+
+// backdrop.addEventListener('click', function() {
+//     modal.style.display = 'none';
+//     backdrop.style.display = 'none';
+// });
+
+// modalNoBtn.addEventListener('click', function() {
+//     modal.style.display = 'none';
+//     backdrop.style.display = 'none';
+// });
+
+// best way
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+};
+
+backdrop.addEventListener('click', closeModal);
+
+modalNoBtn.addEventListener('click', closeModal);
