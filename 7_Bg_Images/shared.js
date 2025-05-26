84442c4f51d3a80ae6d049a4620686042d175bbc
Pulkit Rathi <pulkitr2002@gmail.com>
2025-05-26 22:41:27 +0530

    completed navbar

--- 7_Bg_Images/shared.js
+++ 7_Bg_Images/shared.js
@@ -2,15 +2,24 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoBtn = document.querySelector('.modal_action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleBtn = document.querySelector('.toggle-button');
+var mobNav = document.querySelector('.mobile-nav');
 
 // console.dir(backdrop);
+// console.dir(backdrop.style.zIndex);
+// console.dir(backdrop.style['z-index']);
 
 // backdrop.style.display = 'block';
 
 for (var i=0; i<selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+
+        // modal.className = 'open'; // This will actually overwrite the complete class list
+
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
@@ -27,10 +36,24 @@ for (var i=0; i<selectPlanButtons.length; i++) {
 // best way
 
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 };
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function(){
+    // mobNav.style.display = 'none';
+    mobNav.classList.remove('open');
+    closeModal();
+});
 
 modalNoBtn.addEventListener('click', closeModal);
+
+toggleBtn.addEventListener('click', function(){
+    // mobNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobNav.classList.add('open');
+    backdrop.classList.add('open');
+});
